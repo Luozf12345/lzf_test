387d22290798a8d6fb887ce13d7e52a1effcb865
zflyluo <zflyluo@tencent.com>
2023-12-04 16:30:44 +0800

    init2

---
+++ example/lzf_test_example.dart
@@ -0,0 +1,6 @@
+import 'package:lzf_test/lzf_test.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/lzf_test_test.dart
@@ -0,0 +1,16 @@
+import 'package:lzf_test/lzf_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
